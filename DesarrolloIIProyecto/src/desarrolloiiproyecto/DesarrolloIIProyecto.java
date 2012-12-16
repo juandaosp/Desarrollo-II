d112543eed184791e8050b9594d6a9c906316881
Juan David Ospina <juandaosp12@gmail.com>
2012-12-16 00:55:47 -0500

    modificacion

--- DesarrolloIIProyecto/src/desarrolloiiproyecto/DesarrolloIIProyecto.java
+++ DesarrolloIIProyecto/src/desarrolloiiproyecto/DesarrolloIIProyecto.java
@@ -15,7 +15,7 @@ public class DesarrolloIIProyecto {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         // TODO code application logic here
         
         LoginPrincipal LP = new LoginPrincipal();
